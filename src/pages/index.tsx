70f5a85b907995ff714930bcd59d2be39f9cb713
Hansol <102638663+katf101@users.noreply.github.com>
2023-06-06 16:57:24 +0900

    home - 최상위 div 반응형 설정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,11 +6,15 @@ import Header from "@/components/Header";
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div
+      className="relative h-screen bg-gradient-to-b from-gray-900/10
+    to-[#010511] lg:h-[140vh]"
+    >
       <Head>
         <title>Home - Netflix</title>
         <link rel="icon" href="" />
       </Head>
+
       <Header />
       <main>
         {/* Banner */}
