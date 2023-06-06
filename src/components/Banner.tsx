e396a4c4980f45f35ac5f7c297323997231953f4
Hansol <102638663+katf101@users.noreply.github.com>
2023-06-07 01:12:55 +0900

    메인 베너 구현

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,15 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import { Movie } from "../../globals";
+import { baseUrl } from "@/consts/movie";
 
 interface Props {
   netflixOriginals: Movie[];
 }
 
 export default function Banner({ netflixOriginals }: Props) {
+  const [movie, setMovie] = useState<Movie | null>(null);
+
+  useEffect(() => {
+    setMovie(
+      // 배열의 길이 내에서 랜덤한 정수를 반환
+      netflixOriginals[Math.floor(Math.random() * netflixOriginals.length)]
+    );
+  }, [netflixOriginals]);
+
+  console.log(movie);
+
   return (
-    <div>
-      <div>{/* <Image /> */}</div>
+    <div className="flex flex-col space-y-2 py-16 md:space-y-4 lg:h-[65vh] lg:justify-end lg:pb-12">
+      <div className="absolute top-0 left-0 -z-10 h-[95vh] w-screen">
+        <Image
+          src={`${baseUrl}${movie?.backdrop_path || movie?.poster_path}`}
+          layout="fill"
+          objectFit="cover"
+        />
+      </div>
+
+      <h1 className="text-2xl lg:text-7xl md:text-4xl font-bold">
+        {movie?.title || movie?.name || movie?.original_name}
+      </h1>
+      <p className="max-w-xs text-xs md:max-w-lg md:text-lg lg:max-w-2xl lg:text-2xl ">
+        {movie?.overview}
+      </p>
+
+      <div>
+        <button className="bannerButton">Play</button>
+        <button className="bannerButton">More Info</button>
+      </div>
     </div>
   );
 }
