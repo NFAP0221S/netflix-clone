97192f9599a30db43e894b240fe95a99e689ee5a
Hansol <102638663+katf101@users.noreply.github.com>
2023-06-11 23:57:30 +0900

    next.config 도메인 추가

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,7 +1,22 @@
+import Head from "next/head";
+import Image from "next/image";
 import React from "react";
 
 function login() {
-  return <div>login</div>;
+  return (
+    <div>
+      <Head>
+        <title>Netflix</title>
+        <link rel="icon" href="" />
+      </Head>
+      <Image
+        src="https://rb.gy/p2hphi"
+        layout="fill"
+        className="-z-10 !hidden opacity-60 sm:!inline"
+        objectFit="cover"
+      />
+    </div>
+  );
 }
 
 export default login;
